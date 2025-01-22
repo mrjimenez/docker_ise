24552ef268e013008021aa22303bf75a4f0a0473
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2025-01-21 23:18:22 -0300

    reformat

--- createcontainer.sh
+++ createcontainer.sh
@@ -5,9 +5,12 @@
 tmux new -s createdocker -d "python3 -m http.server"
 
 # build docker
-docker build --build-arg SERVER_HOST=host.docker.internal:8000 \
+docker build \
+        --build-arg SERVER_HOST=host.docker.internal:8000 \
         --add-host=host.docker.internal:host-gateway \
-        -f Dockerfile -t xilinx_ise .
+        -f Dockerfile \
+        -t xilinx_ise \
+        .
 
 # close tmux session
 tmux kill-session -t createdocker
